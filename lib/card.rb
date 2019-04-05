52334470db1d231d8b8cbeca1382ab3f30d65181
Brandon Weygant <bobbydigital108@gmail.com>
2019-04-05 11:04:10 -0400

    Removed blocked out unused code for a cleaner look.

--- lib/card.rb
+++ lib/card.rb
@@ -1,13 +1,10 @@
 class Card
 attr_accessor :name, :program, :ovr, :position, :price, :href
 
-# PLAYER_CARDS = []
-
   def initialize(card_hash)
     card_hash.each do |key, value|
       self.send("#{key}=", value)
     end
-    # PLAYER_CARDS << self unless PLAYER_CARDS.include?(self)
   end
 
   def self.create_from_array(card_array)
@@ -15,15 +12,4 @@ attr_accessor :name, :program, :ovr, :position, :price, :href
       Card.new(card_hash)
     end
   end
-
-  # def self.all
-  #   PLAYER_CARDS
-  # end
-
-  # def save
-  #   PLAYER_CARDS << self unless PLAYER_CARDS.include?(self)
-  # end
-#binding.pry
-
-#all
 end
