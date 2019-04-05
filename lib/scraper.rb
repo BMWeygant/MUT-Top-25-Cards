94c3b52bdaa4d397b0458b9738bbe9f47b6f5233
Brandon Weygant <bobbydigital108@gmail.com>
2019-04-05 11:02:42 -0400

    Removed a rogue pry. Unblocked unused href scraper for future use.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,13 +12,12 @@ BASE_URL = "https://www.muthead.com/19/players"
 
       doc = Nokogiri::HTML(open(BASE_URL))
       doc.css("tr.even, tr.odd").each do |details|
-        #binding.pry
         name = details.css("a").text.strip
         program = details.css("span").text.strip
         ovr = details.css(".col-overall").text.strip
         position = details.css(".col-position").text.strip
         price = details.css(".col-price").text.strip
-        #href =  details.css("a").attribute("href").value
+        href =  details.css("a").attribute("href").value
         card_array << {
           name: name,
           program: program,
